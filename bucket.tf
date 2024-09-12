3f729913048c2334120cb2d24cc729bde31b2324
mankrome <manuel.kromer@deutschepost.de>
2024-09-12 11:36:07 +0200

    Task 1 and 2

--- bucket.tf
+++ bucket.tf
@@ -2,9 +2,20 @@
 
 # TODO: Create a google_storage_bucket 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
+resource "google_storage_bucket" "my-bucket" {
+  name          = "manuel-bucket"
+  location      = "EU"
+}
 
 # TODO: Create an IAM binding with the role Storage Object User and yourself as the member
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket_iam
+resource "google_storage_bucket_iam_binding" "binding" {
+  bucket = google_storage_bucket.my-bucket.name
+  role = "roles/storage.admin"
+  members = [
+    "user:manuel.kromer@senacor.com",
+  ]
+}
 
 # TODO: Create a data source for a GCS Bucket and use following bucket: hack-and-heat-2024-task-5
 # A data source allows Terraform to use information defined outside of Terraform, in this case our already exisiting bucket in the GCP project.
--- service_account.tf
+++ service_account.tf
@@ -1,8 +1,16 @@
 # TODO: Create a service account
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account
+resource "google_service_account" "manuel_service_account" {
+  account_id   = "service-account-manuel"
+  display_name = "Service Account"
+}
 
 # TODO: Create a service account key
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account_key
+resource "google_service_account_key" "mykey" {
+  service_account_id = google_service_account.manuel_service_account.name
+  public_key_type    = "TYPE_X509_PEM_FILE"
+}
 
 # TODO: Use the terraform cli to print the state with sensitive values hidden
 # terraform show
